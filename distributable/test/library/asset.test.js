748dd09168969d8ae56844371f278a8400093121
virtualpatterns <code.virtualpatterns@gmail.com>
2020-07-05 12:48:21 -0400

    post-test

--- distributable/test/library/asset.test.js
+++ distributable/test/library/asset.test.js
@@ -14,7 +14,7 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-(0, _ava.default)('import(require.resolve(\'./target/target.js\')', async test => {
+(0, _ava.default)('Bundler(require.resolve(\'./source/source.js\'), { ... })', async test => {
   let bundler = new _parcelBundler.default(require.resolve('./source/source.js'), {
     'cache': false,
     'outDir': `${__dirname}/target`,
